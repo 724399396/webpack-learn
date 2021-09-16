e5b8f66d84c4f34ad772a6860973087a1e2e8585
Wei Li <liwei72439936@gmail.com>
2021-09-16 09:37:45 +0800

    composing configuration

---
+++ webpack.parts.js
@@ -0,0 +1,29 @@
+const {
+	WebpackPluginServe
+} = require("webpack-plugin-serve")
+const {
+	MiniHtmlWebpackPlugin,
+} = require("mini-html-webpack-plugin")
+
+exports.devServer = () => ({
+	watch: true,
+	plugins: [
+		new WebpackPluginServe({
+			host: "localhost",
+			port: process.env.PORT || 8080,
+			static: "./dist",
+			liveReload: true,
+			waitForBuild: true,
+		}),
+	],
+});
+
+exports.page = ({
+	title
+}) => ({
+	plugins: [new MiniHtmlWebpackPlugin({
+		context: {
+			title
+		}
+	})],
+})
